937a46d561b9952d6bbcf25cbf7ceb2f8b0e91ce
Bielpg04 <94679167+Bielpg04@users.noreply.github.com>
2021-11-26 15:47:38 +0100

    Front-end Actualizado
    Se ha cambiado el diseño de la página web y se ha mejorado alguna característica del JS.

--- js.js
+++ js.js
@@ -19,13 +19,16 @@ function bum1(){
 function bum2(){
     if (document.getElementById('4tecnic').style.display == "block"){
         document.getElementById('4tecnic').style.display = "none";
+        document.getElementById('EZGLPI-F').value="null";
     }
     else{
         if (document.getElementById('3tecnic').style.display == "block"){
             document.getElementById('3tecnic').style.display = "none";
+            document.getElementById('EZGLPI-E').value="null";
         }
         else{
         document.getElementById('2tecnic').style.display = "none";
+        document.getElementById('EZGLPI-D').value="null";
         }
     }
 }
